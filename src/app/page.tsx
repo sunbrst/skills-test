fb7fdaca6fa03ca24ab422020fd34423732f4506
zachmeza <zach.meza@quindar.space>
2023-03-23 12:22:06 -0400

    Initial table for launch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,29 @@ import styles from "./page.module.css";
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import App, { AppContext, AppProps } from "next/app";
 import { useEffect, useState } from "react";
+import { Table } from "antd"
 
 const inter = Inter({ subsets: ["latin"] });
 
 
 export default function Home() {
-  const [data, setData] = useState();
+  const [data, setData] = useState<any[]>([])
+
+
+  const columns = [
+    {
+      title: 'Mission Name',
+      dataIndex: 'mission_name',
+      key: 'mission_name',
+    },
+    {
+      title: 'Launch Date',
+      dataIndex: 'launch_date_local',
+      key: 'launch_date_local',
+    }
+  ];
+
+
   useEffect(() => {
     async function fetchData() {
       const client = new ApolloClient({
@@ -39,11 +56,7 @@ export default function Home() {
           }
         `
       });
-      // const res = await fetch(
-      //   "https://proton.api.atomicassets.io/atomicmarket/v1/sales"
-      // );
-      // const { data } = await res.json();
-      setData(data);
+      setData(data["launchesPast"]);
     }
     fetchData();
   }, []);
@@ -54,83 +67,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+       <Table columns={columns} dataSource={data} />
       </div>
     </main>
   );
